7582f68da65f9003c984a392f6eec83d2161d018
Monika Kakuturu <monikakakuturu@gmail.com>
2020-09-12 19:44:41 -0700

    Implemented the functionality to get the team information and create html file

--- app.js
+++ app.js
@@ -9,27 +9,158 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
+const Employee = require("./lib/Employee");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+const managerQues = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the Manager's name?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the Manager's email?",
+  },
+  {
+    type: "input",
+    name: "officenumber",
+    message: "What is the Manager's office number?",
+  },
+  {
+    type: "list",
+    message: "What type of team member you want to add?",
+    name: "type",
+    choices: ["Engineer", "Intern", "No, I am done adding"],
+  },
+];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+const engineerQues = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the Engineer's name?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the Engineer's email?",
+  },
+  {
+    type: "input",
+    name: "githublink",
+    message: "What is the Engineer's github profile?",
+  },
+  {
+    type: "list",
+    message: "What type of team member you want to add?",
+    name: "type",
+    choices: ["Engineer", "Intern", "No, I am done adding"],
+  },
+];
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+const internQues = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the Intern's name?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the Intern's email?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What is the Intern's school name?",
+  },
+  {
+    type: "list",
+    message: "What type of team member you want to add?",
+    name: "type",
+    choices: ["Engineer", "Intern", "No, I am done adding"],
+  },
+];
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+var employeeList = [];
+// employee id is unique so update it programatically instead of user entered value
+var employeeID = 1;
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+// function to initialize program and gets the manager info
+function initManager() {
+  employeeList.length = 0;
+  inquirer.prompt(managerQues).then(function (data) {
+    const manager = new Manager(
+      data.name,
+      employeeID,
+      data.email,
+      data.officeNumber
+    );
+    employeeID++;
+    employeeList.push(manager);
+    checkEmployeeType(data.type);
+  });
+}
+
+// gets the engineer information
+function EngineerInfo() {
+  inquirer.prompt(engineerQues).then(function (data) {
+    const engineer = new Engineer(
+      data.name,
+      employeeID,
+      data.email,
+      data.github
+    );
+    employeeID++;
+    employeeList.push(engineer);
+    checkEmployeeType(data.type);
+  });
+}
+
+// gets the intern information
+function InternInfo() {
+  inquirer.prompt(internQues).then(function (data) {
+    const intern = new Intern(data.name, employeeID, data.email, data.school);
+    employeeID++;
+    employeeList.push(intern);
+    checkEmployeeType(data.type);
+  });
+}
+
+// checks the employee type and continue to enter info until done
+function checkEmployeeType(type) {
+  switch (type) {
+    case "Engineer":
+      EngineerInfo();
+      break;
+    case "Intern":
+      InternInfo();
+      break;
+    default:
+      createHTML();
+      break;
+  }
+}
+
+// renders the employee list and generates output html file
+function createHTML() {
+  const html = render(employeeList);
+  // check if directory exists and create if not exists
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+
+  fs.writeFile(outputPath, html, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully created the Team profile file!");
+    }
+  });
+}
+
+// function call to initialize program
+initManager();
